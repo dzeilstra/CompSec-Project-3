31c9eea8317900e1a4d267978854e4c87bb8be21
Zeilstra <zeilstraac20@gcc.edu>
2024-12-05 15:27:10 -0500

    Added instructions in comments

--- decryption.py
+++ decryption.py
@@ -1,9 +1,20 @@
 import os
 from cryptography.hazmat.primitives.ciphers import Cipher, modes, algorithms
 
+
+
 for i in range(109583):
+    # Read .txt file as a .txt file
+    # Read it line by line and strip all of the newline characters
+    # .rstrip()
+
+    # Then pad them out if they are less than 16 chars and truncate if longer
+    # Then once padded satisfactorily, use .encode() to convert the string object into a byte string
+    # Then I can use it as a key
     key = "PLACEHOLDER"
     iv = b"0"*16
     cipher = Cipher(algorithms.AES128(key), modes.CBC(iv))
     decryptor = cipher.decryptor()
-    decryptor.update("PLACEHOLDER") + decryptor.finalize()
+    # Read the encrypted file as a binary using RB mode (how to read a Python file in binary)
+    result = decryptor.update("PLACEHOLDER") + decryptor.finalize()
+    print(result)
