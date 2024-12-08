b72a1f978b5c441183e32ded83bb065ff281a2dc
dzeilstra <alexanderzeilstra2002@gmail.com>
2024-12-07 21:06:39 -0500

    Added truncation and padding

--- decryption.py
+++ decryption.py
@@ -2,19 +2,44 @@ import os
 from cryptography.hazmat.primitives.ciphers import Cipher, modes, algorithms
 
 
+# Read .txt file as a .txt file in Python
+# Read it line by line and strip all of the newline characters
+# .rstrip()
+msg = open("encrypted2.txt", "r", encoding="ANSI")
+list1 = list()
+for x in msg:
+    print(x.rstrip())
+    list1.append(x.rstrip())
 
-for i in range(109583):
-    # Read .txt file as a .txt file
-    # Read it line by line and strip all of the newline characters
-    # .rstrip()
+complete = list1[0]+list1[1]
+print(complete)
 
+#print(list)
+
+words = open("wordsEn.txt", "r")
+list2 = list()
+for word in words:
+    if len(word) == 16:
+        word = word.rstrip()
+    if len(word) < 16:
+        word = word.rstrip()
+        word = word.ljust(16)
+    if len(word) > 16:
+        word = word.rstrip()
+        word = word[:16]
+    list2.append(word)
+
+print(list2)
+
+for word in list2:
     # Then pad them out if they are less than 16 chars and truncate if longer
     # Then once padded satisfactorily, use .encode() to convert the string object into a byte string
     # Then I can use it as a key
-    key = "PLACEHOLDER"
+
+    key = word.encode()
     iv = b"0"*16
     cipher = Cipher(algorithms.AES128(key), modes.CBC(iv))
     decryptor = cipher.decryptor()
     # Read the encrypted file as a binary using RB mode (how to read a Python file in binary)
-    result = decryptor.update("PLACEHOLDER") + decryptor.finalize()
-    print(result)
+    #result = decryptor.update("PLACEHOLDER") + decryptor.finalize()
+    #print(result)
